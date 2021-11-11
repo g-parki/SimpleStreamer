d39958ec62e8be85bc06b08b5bb16513a858b1eb
g-parki <61096711+g-parki@users.noreply.github.com>
2021-11-11 15:18:48 -0800

    Add support for environment var options

--- run.py
+++ run.py
@@ -1,17 +1,22 @@
 import argparse
+import os
 from scripts import app
 
 PORT_DEFAULT = 5700
 PORT_ALT = 5705
 
 parser = argparse.ArgumentParser("Run streaming app")
-parser.add_argument("--windows", help="Run with Windows socket interface", action="store_true")
+parser.add_argument("--socket", help="Run with socket interface", action="store_true")
 parser.add_argument("--alt", help=f"Run on alternative port {PORT_ALT}", action="store_true")
 args = parser.parse_args()
 
-port = PORT_DEFAULT if not args.alt else PORT_ALT
-app.with_socket_client = args.windows
+if args.alt or os.environ.get("ALT") == "True":
+    port = PORT_ALT
+else: 
+    port = PORT_DEFAULT
+
+if args.socket or os.environ.get('SOCKET') == "True":
+    app.with_socket_client = True
 
 if __name__ == '__main__':
-    
     app.run(host='0.0.0.0', port=port, debug=False)
